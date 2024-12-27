bfd37a556235e79eeca471910dc7c382cca8cb37
satheeshMsakkaa <139038595+satheeshMsakkaa@users.noreply.github.com>
2024-12-27 19:25:24 +0530

    Delete app-mfe directory

--- app-mfe/src/app/mfe/link1.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mfe',
-  template: `<h1>Welcome to SCM Micro Front End ! => Request Component loaded</h1>`,
-})
-export class Link1Component {}
